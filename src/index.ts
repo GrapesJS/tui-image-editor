1cee2a13b1c4447a2b42877e9405de61c3cfcee6
Artur Arseniev <artur.catch@hotmail.it>
2022-08-22 16:52:32 +0200

    Add TS plugin options

--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,122 @@
-export default (editor, options = {}) => {
-  const opts = {
-    // TOAST UI's configurations
-    // https://nhn.github.io/tui.image-editor/latest/ImageEditor
+import type grapesjs from 'grapesjs';
+
+export type PluginOptions = {
+  /**
+   * TOAST UI's configurations
+   * https://nhn.github.io/tui.image-editor/latest/ImageEditor
+   */
+  config?: Record<string, any>;
+
+  /**
+   * Pass the editor constructor.
+   * By default, the `tui.ImageEditor` will be used.
+   */
+  constructor?: any;
+
+  /**
+   * Label for the image editor (used in the modal)
+   * @default 'Image Editor'
+   */
+  labelImageEditor?: string;
+
+  /**
+   * Label used on the apply button
+   * @default 'Apply'
+   */
+  labelApply?: string;
+
+  /**
+   * Default editor height
+   * @default '650px'
+   */
+  height?: string;
+
+  /**
+   * Default editor width
+   * @default '100%'
+   */
+  width?: string;
+
+  /**
+   * Id to use to create the image editor command
+   * @default 'tui-image-editor'
+   */
+  commandId?: string;
+
+  /**
+   * Icon used in the component toolbar
+   */
+  toolbarIcon?: string;
+
+  /**
+   * Hide the default editor header
+   * @default true
+   */
+  hideHeader?: boolean;
+
+  /**
+   * By default, GrapesJS takes the modified image, adds it to the Asset Manager and update the target.
+   * If you need some custom logic you can use this custom 'onApply' function.
+   * @example
+   * onApply: (imageEditor, imageModel) => {
+   *    const dataUrl = imageEditor.toDataURL();
+   *    editor.AssetManager.add({ src: dataUrl }); // Add it to Assets
+   *    imageModel.set('src', dataUrl); // Update the image component
+   * }
+   */
+  onApply?: (imageEditor: any, imageModel: any) => void;
+
+  /**
+   * If no custom `onApply` is passed and this option is `true`, the result image will be added to assets
+   * @default true
+   */
+   addToAssets?: boolean;
+
+   /**
+    * If no custom `onApply` is passed, on confirm, the edited image, will be passed to the
+    * AssetManager's uploader and the result (eg. instead of having the dataURL you'll have the URL)
+    * will be passed to the default `onApply` process (update target, etc.)
+    */
+   upload?: boolean;
+
+   /**
+    * The apply button (HTMLElement) will be passed as an argument to this function, once created.
+    * This will allow you a higher customization.
+    */
+   onApplyButton?: (btn: HTMLElement) => void;
+
+   /**
+    * Scripts to load dynamically in case no TOAST UI editor instance was found
+    */
+   script?: string[];
+   /**
+    * In case the script is loaded this style will be loaded too
+    */
+   style?: string[];
+};
+
+const plugin: grapesjs.Plugin<PluginOptions> = (editor, options = {}) => {
+  const opts: PluginOptions = {
     config: {},
-
-    // Pass the editor constructor. By default, the `tui.ImageEditor` will be called
     constructor: '',
-
-    // Label for the image editor (used in the modal)
     labelImageEditor: 'Image Editor',
-
-    // Label used on the apply button
     labelApply: 'Apply',
-
-    // Default editor height
     height: '650px',
-
-    // Default editor width
     width: '100%',
-
-    // Id to use to create the image editor command
     commandId: 'tui-image-editor',
-
-    // Icon used in the component toolbar
     toolbarIcon: `<svg viewBox="0 0 24 24">
                     <path d="M20.71 7.04c.39-.39.39-1.04 0-1.41l-2.34-2.34c-.37-.39-1.02-.39-1.41 0l-1.84 1.83 3.75 3.75M3 17.25V21h3.75L17.81 9.93l-3.75-3.75L3 17.25z">
                     </path>
                   </svg>`,
-
-    // Hide the default editor header
-    hideHeader: 1,
-
-    // By default, GrapesJS takes the modified image, adds it to the Asset Manager and update the target.
-    // If you need some custom logic you can use this custom 'onApply' function
-    // eg.
-    // onApply: (imageEditor, imageModel) => {
-    //   const dataUrl = imageEditor.toDataURL();
-    //   editor.AssetManager.add({ src: dataUrl }); // Add it to Assets
-    //   imageModel.set('src', dataUrl); // Update the image component
-    // }
-    onApply: 0,
-
-    // If no custom `onApply` is passed and this option is `true`, the result image will be added to assets
-    addToAssets: 1,
-
-    // If no custom `onApply` is passed, on confirm, the edited image, will be passed to the AssetManager's
-    // uploader and the result (eg. instead of having the dataURL you'll have the URL) will be
-    // passed to the default `onApply` process (update target, etc.)
-    upload: 0,
-
-    // The apply button (HTMLElement) will be passed as an argument to this function, once created.
-    // This will allow you a higher customization.
+    hideHeader: true,
+    addToAssets: true,
+    upload: false,
     onApplyButton: () => {},
-
-    // Scripts to load dynamically in case no TOAST UI editor instance was found
     script: [
       'https://uicdn.toast.com/tui.code-snippet/v1.5.2/tui-code-snippet.min.js',
       'https://uicdn.toast.com/tui-color-picker/v2.2.7/tui-color-picker.min.js',
       'https://uicdn.toast.com/tui-image-editor/v3.15.2/tui-image-editor.min.js'
     ],
-
-    // In case the script is loaded this style will be loaded too
     style: [
       'https://uicdn.toast.com/tui-color-picker/v2.2.7/tui-color-picker.min.css',
       'https://uicdn.toast.com/tui-image-editor/v3.15.2/tui-image-editor.min.css',
@@ -69,27 +125,31 @@ export default (editor, options = {}) => {
   };
 
   const { script, style, height, width, hideHeader, onApply, upload, addToAssets, commandId } = opts;
-  const getConstructor = () => opts.constructor || window?.tui?.ImageEditor;
+  const getConstructor = () => {
+    return opts.constructor ||
+      // @ts-ignore
+      window?.tui?.ImageEditor;
+  };
   let constr = getConstructor();
 
   // Dynamic loading of the image editor scripts and styles
   if (!constr && script?.length) {
     const { head } = document;
     const scripts = Array.isArray(script) ? [...script] : [script];
-    const styles = Array.isArray(style) ? [...style] : [style];
-    const appendStyle = styles => {
+    const styles = (Array.isArray(style) ? [...style] : [style]) as string[];
+    const appendStyle = (styles: string[]) => {
       if (styles.length) {
         const link = document.createElement('link');
-        link.href = styles.shift();
+        link.href = styles.shift()!;
         link.rel = 'stylesheet';
         head.appendChild(link);
         appendStyle(styles);
       }
     }
-    const appendScript = scripts => {
+    const appendScript = (scripts: string[]) => {
       if (scripts.length) {
         const scr = document.createElement('script');
-        scr.src = scripts.shift();
+        scr.src = scripts.shift()!;
         scr.onerror = scr.onload = appendScript.bind(null, scripts);
         head.appendChild(scr);
       } else {
@@ -249,3 +309,5 @@ export default (editor, options = {}) => {
     },
   });
 };
+
+export default plugin;
